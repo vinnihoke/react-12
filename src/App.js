a818f1eaad86048d6fab024f3d5e4c0969d7e4d7
Vinni Hoke <vinni.hoke@gmail.com>
2019-09-22 20:49:46 -0700

    Finishing completed in reducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.scss";
+import Todo from "./components/Todo.js";
 
 function App() {
   return (
     <div className="App">
       <h1>React App Ready</h1>
+      <Todo />
     </div>
   );
 }
